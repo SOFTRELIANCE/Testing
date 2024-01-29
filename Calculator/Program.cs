5aea69eb67bf552f77a274d7b577bf74741d2b54
unknown <prince.brown@softreliancegh.com>
2024-01-29 09:38:29 +0000

    First commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            Console.WriteLine("Input the first Number:");
+            string firstNumber = Console.ReadLine();
+
+
+            Console.WriteLine("Input the second Number:");
+            string secondNumber = Console.ReadLine();
+
+            Console.WriteLine("What do you want to do with those numbers?");
+            Console.WriteLine("[A]dd");
+            Console.WriteLine("[S]ubstract");
+            Console.WriteLine("[M]ultiply");
+
+            string userInput = Console.ReadLine();
+
+            if (userInput.ToUpper() == "A" )
+            {
+                Console.WriteLine($"{firstNumber} + {secondNumber} = {(int.Parse(firstNumber) + int.Parse(secondNumber))}");
+            }
+            else if (userInput.ToUpper() == "S")
+            {
+                Console.WriteLine($"{firstNumber} - {secondNumber} = {(int.Parse(firstNumber) - int.Parse(secondNumber))}");
+            }
+            else if (userInput.ToUpper() == "M" )
+            {
+                Console.WriteLine($"{firstNumber} * {secondNumber} = {(int.Parse(firstNumber) * int.Parse(secondNumber))}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid option");
+            }
+
+
+            Console.WriteLine("Press any Key to close");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rectangle1 = new Rectangle(3,4);
+            //var calculate = new ComputeShapeMeasurement();
+
+            rectangle1.Width = 6;
+            rectangle1.Height = 7;
+            Console.WriteLine($"Width of Rectangle is {rectangle1.Width}");
+            Console.WriteLine($"Height of Rectangle is {rectangle1.Height}");
+
+            Console.WriteLine($"Area of Rectangle is {rectangle1.CalcAreaOfRectangle()}");
+            Console.WriteLine($"Perimeter of Rectangle is {rectangle1.CalcPerimeterOfRectangle()}");
+
+            Console.ReadKey();
+        }
+        class Rectangle
+        {
+            private int _width;
+            private int _height;
+
+            public Rectangle(int width,int height)
+            {
+                _width = width;
+                _height = height;
+            }
+
+            public int Width
+            {
+                get
+                {
+                    return _width;
+
+                }
+                set
+                {
+                    if (value > 0)
+                        {
+                        _width = value;
+                        }
+                }                
+
+            }
+
+            public int Height
+            {
+                get
+                {
+                    return _height;
+                }
+               
+                set
+                {
+                    if(value > 0)
+                    {
+                        _width = value;
+                    }
+                }
+            }
+
+            /*
+            public int GetWidth()
+            {
+                return Width;
+            }
+
+            public void SetWidth(int value)
+            {
+                if(value > 0 )
+                Width = value;
+            }
+            */
+
+            public int CalcAreaOfRectangle()
+            {
+                return Width * Height;
+            }
+
+            public int CalcPerimeterOfRectangle()
+            {
+                return 2 * (Width + Height);
+            }
+
+        }
+
+          /* 
+         class ComputeShapeMeasurement
+        {
+            public int CalcAreaOfRectangle(Rectangle rectangle)
+            {
+                return rectangle.Width * rectangle.Height;
+            }
+
+            public int CalcPerimeterOfRectangle(Rectangle rectangle)
+            {
+                return 2 * (rectangle.Width + rectangle.Height);
+            }
+        }
+
+           */
+        
+
+       
+    }
+}
